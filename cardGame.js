1a0d4127143497b1b12c4aa43565aebe5fd5b83c
Bruno Alves <brunoalvestrajano250@gmail.com>
2023-12-03 09:33:35 -0300

    :boom: function attDeck deleted
    :sparkles: new function playCard

--- cardGame.js
+++ cardGame.js
@@ -1,4 +1,4 @@
-const deck = ["cavaleiro", "aventureiro", "alquimista", "lobisomem", "caçador de almas", "feiticeiro", "bruxo", "mecânico", "clérigo"]
+const deck = ["cavaleiro", "cavaleiro", "cavaleiro", "aventureiro", "alquimista", "lobisomem", "caçador de almas", "feiticeiro", "bruxo", "mecânico", "clérigo"]
 const hand = []
 const discardPile = []
 
@@ -7,21 +7,26 @@ function draw(card) {
   hand.push(cardDrawn)
 }
 
-function attDeck(removed) {
-  let [retiredCard] = deck.splice(removed, 1)
+function playCard(retiredCard) {
+  console.table({ Hand_before_play: hand })
+  setTimeout(() => {
+    console.log(`Carta jogada: ${retiredCard}`);
+  }, 1000)
+  hand.splice(hand.indexOf(retiredCard), 1)
   discardPile.push(retiredCard)
 }
 
 function shuffle(decks) {
-  for (let i = 0; i <= 3; i++) {
+  for (let handLength = 0; handLength <= 3; handLength++) {
     let shuffled = Math.floor(Math.random() * decks.length)
     draw(shuffled)
-    attDeck(shuffled)
+    deck.splice(shuffled, 1)
   }
 }
 
 shuffle(deck)
+playCard(hand[2])
 
-// console.table({ hand })
-// console.table({ deck_atual: deck })
-// console.table({ pilha_de_descaste: discardPile })
+console.table({ hand })
+console.table({ deck_atual: deck })
+console.table({ pilha_de_descaste: discardPile })
